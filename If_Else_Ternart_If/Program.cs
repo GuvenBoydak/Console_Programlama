afcbe09120e1138f14717613b0da46c970d1bcf5
güven boydak <gvn.boydak@gmail.com>
2022-01-27 19:33:51 +0300

    If_Else Ternary _If

---
+++ If_Else_Ternart_If/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace If_Else_Ternart_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >=6 && time <=11)
+            {
+                Console.WriteLine("Günaydin!!");
+            }
+            else if (time <=18)
+            {
+                Console.WriteLine("İyi Günler!!");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler!!");
+            }
+
+            string sonuc = time >= 6 && time <= 11 ? "Günaydin!!" : time <= 18 ? "İyi Günler!!" : "İyi Geceler!!";
+           
+            Console.ReadLine();
+        }
+    }
+}
