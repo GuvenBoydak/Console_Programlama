4a11af4c6c5d15f6c59d0bdf7c3210383652e76a
güven boydak <gvn.boydak@gmail.com>
2022-02-04 13:59:03 +0300

    Method_Overloading

---
+++ Method_Overloading/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Out parametreler
+            string sayi = "999";
+
+            bool sonuc=int.TryParse(sayi, out int OutSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(OutSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız!!");
+            }
+
+            Metodlar metodlar = new Metodlar();
+            metodlar.Topla(5, 5, out int toplaSonuc);
+            metodlar.EkranaYazdır(Convert.ToString(toplaSonuc));
+
+
+            //Metot Aşırı yükleme - Overloading
+
+            int ifade = 999;
+            metodlar.EkranaYazdır(ifade );
+            metodlar.EkranaYazdır("Güven", "Boydak");
+
+        }
+    }
+
+    class Metodlar
+    {
+        public void Topla(int deger1,int deger2,out int topla)
+        {
+            topla = deger1 + deger2;
+        }
+
+        public void EkranaYazdır(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdır(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public void EkranaYazdır(string veri1,string veri2)
+        {
+            Console.WriteLine(veri1 + veri2);
+        }
+    }
+}
